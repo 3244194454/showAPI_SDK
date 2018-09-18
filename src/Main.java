fdc441e43f8bd519f148352856548a0397efa013
dj-thinkpad570 <3244194454@qq.com>
2018-09-18 15:28:16 +0800

    API调用示例

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        String appid="";//应用ID
-        String secret="";//应用密钥
+        String appid="";//应用ID,请在showapi.com-->个人中心-->我是接口使用者->我的应用 中查找
+        String secret="";//应用密钥 
         //调用示例
         String res=new ShowApiRequest("http://route.showapi.com/66-22", appid, secret)
                 .addTextPara("code","6938166920785")
